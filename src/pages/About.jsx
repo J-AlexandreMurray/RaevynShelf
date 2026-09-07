0372b46a97816f32c1170d1d2e8244b69be73e38
J. A. Murray <77152103+J-AlexandreMurray@users.noreply.github.com>
2026-09-07 13:30:39 +0200

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,15 +6,25 @@ export default function About() {
     <h1>Made for AO3 readers.</h1>
     <p>RaevynTide Library is an independent, AO3-only reading companion for readers that wish to keep up with their own private shelf, see their everchanging reading statistics, and even optionally share a 'reading stat' card to others.</p>
     <p><strong>RaevynTide Library is not affiliated with, endorsed by, or operated by the Organization for Transformative Works or Archive of Our Own. The RaevynTide Project is purely an indipendent project created and supported by a fan of the community.</strong></p>
+
     <h2>Support development</h2>
-    <p>The RaevynTide Library app is intended to remain free to use indefinitely. If you enjoy it, you can optionally support its independent development through Ko-fi.</p>
-    {kofiUrl && (
+
+<p>
+  The RaevynTide Library app is intended to remain free to use indefinitely.
+  If you enjoy it, you can optionally support its independent development
+  through Ko-fi.
+</p>
+
+{kofiUrl && (
   <a
     className="primary"
     href={kofiUrl}
     target="_blank"
     rel="noopener noreferrer"
   >
-    ☕ Support Development
-  </main>;
+    ☕ Support development
+  </a>
+)}
+</main>
+);
 }
